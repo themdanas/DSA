cde1c9de96c36c42683eeee704cfaca250f6e93e
themdanas <162160204+themdanas@users.noreply.github.com>
2025-08-08 10:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.32%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int max=0;
+        int min=0;
+        for(int i=0; i<s.length(); i++){
+        if(s[i]=='('){
+            min=min+1;
+            max=max+1;
+        }
+        else if(s[i]==')'){
+            min=min-1;
+            max=max-1;
+        }
+        else{
+            min=min-1;
+            max=max+1;
+        }
+        if(min<0) min=0;
+        if(max<0) return false;
+        
+        }
+        return (min == 0);
+    }
+};
