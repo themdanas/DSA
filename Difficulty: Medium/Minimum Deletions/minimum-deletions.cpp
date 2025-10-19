3db6d26a9dcfc77a8ae8ef55cce0e0d49ceb705b
themdanas <162160204+themdanas@users.noreply.github.com>
2025-10-19 21:52:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    int lcs(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        int i,j=0;
+
+        for(i=1; i<=m; i++){
+            for(j=1; j<=n; j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                    dp[i][j]=std::max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+    
+    int lps(string &s) {
+        // code here
+        string s1 = s;            
+        reverse(s1.begin(), s1.end());
+        
+        return lcs(s,s1);
+    }
+    
+    int minDeletions(string &s) {
+        // code here
+        return s.length()-lps(s);
+    }
+};
