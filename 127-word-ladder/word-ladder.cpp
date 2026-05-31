6369d00257759018843d1ee6183e6af3a239759f
themdanas <162160204+themdanas@users.noreply.github.com>
2026-05-31 15:23:47 +0530

    Time: 49 ms (80.10%) | Memory: 21.2 MB (42.71%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,30 +1,28 @@
+//      
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1});
-        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        unordered_set<string> st(wordList.begin(),wordList.end());
         st.erase(beginWord);
-        
         while(!q.empty()){
-            string word=q.front().first;
             int steps=q.front().second;
+            string word=q.front().first;
             q.pop();
-            if(word == endWord) return steps;
-            
+            if(word==endWord) return steps;
+
             for(int i=0; i<word.length(); i++){
                 char original=word[i];
-                for(char ch ='a'; ch<='z'; ch++){
+                for(char ch='a'; ch<='z'; ch++){
                     word[i]=ch;
-                    
-                    if(st.find(word)!= st.end()){
+                    if(st.find(word)!=st.end()){
                         st.erase(word);
-                        q.push({word, steps +1});
+                        q.push({word,steps+1});
                     }
                 }
-                word[i]= original;
+                word[i]=original;
             }
-            
         }
         return 0;
     }
